408d11019235d11b87ccc344ff67b8a5ba36b84a
david zagi <davidzagi93@gmail.com>
2019-08-01 13:09:24 +0100

    adding react app

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -30,7 +30,7 @@ namespace WidgetsApp.Controllers
         {
         }
 
-        // PUT api/values/5
+        // PUT api/values/5 
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
